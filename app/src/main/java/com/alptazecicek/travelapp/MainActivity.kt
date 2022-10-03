7a34e96926cc3f21c356ff3ff428d3bf271c370c
Atzcck <alptazecicek@gmail.com>
2022-10-03 15:24:23 +0300

    home screen statics added. ssp, sdp files added.

--- app/src/main/java/com/alptazecicek/travelapp/MainActivity.kt
+++ app/src/main/java/com/alptazecicek/travelapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.alptazecicek.travelapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
@@ -16,7 +17,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Status bar hide.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN, // will look deprecated.
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
 
+        // Navigation bar initialize.
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         navController = navHostFragment.navController
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavView)
